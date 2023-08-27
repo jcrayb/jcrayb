90bca487d9ec808f89b168186dfd0fde16ed3ea0
jcrayb <jcrb7800@gmail.com>
2023-08-27 11:48:55 -0500

    me 330 lab0

---
+++ python/ME330/lab0/aluminum.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('ME 330 - aluminum.csv')
+
+E = 70895
+offset = 0.002
+
+diff_array = []
+x_array = []
+
+for i in range(len(df['Stress'])):
+    strain = df['Strain 1'][i]
+    stress = df['Stress'][i]
+    
+    offset_f = E*(strain-offset)-stress
+
+    diff_array += [abs(offset_f)]
+    x_array += [strain]
+
+np_diff = np.array(diff_array)
+
+min_index = np.where(np_diff == np.min(np_diff))[0][0]
+print(df['Strain 1'][min_index], df['Stress'][min_index])
+
